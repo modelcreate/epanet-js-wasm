021147d1098ae19d85a8cfe9f78de31ef09840d0
lbutler <lukepbutler@gmail.com>
2025-04-28 10:47:05 -0400

    Long sim benchmark

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,33 @@
 import { benchmarkNodeIndexCalls, benchmarkNodeIndexCallsEpanetJs } from './benchmarks/calls-per-second.js';
 import { benchmarkOpenLargeNetworkWasm, benchmarkOpenLargeNetworkEpanetJs } from './benchmarks/open-large-network.js';
+import { benchmarkRunLongSimWasm, benchmarkRunLongSimEpanetJs } from './benchmarks/run-long-sim.js';
 
 
 // Use it in an async function
 async function runBenchmark() {
     try {
+
+
+        console.log("--------------------------------");
+        console.log("Run Long Sim");
+
+        // Run WASM version
+        const longsimWasmResults = await benchmarkRunLongSimWasm(1);
+        console.log('WASM Results:', {
+            averageDuration: longsimWasmResults.averageDuration,
+            totalDuration: longsimWasmResults.totalDuration
+        });
+
+        // Run epanet-js version
+        const longsimJsResults = await benchmarkRunLongSimEpanetJs(1);
+        console.log('epanet-js Results:', {
+            averageDuration: longsimJsResults.averageDuration,
+            totalDuration: longsimJsResults.totalDuration
+        });
+
+        console.log("--------------------------------");
+        console.log("Node Index Calls");
+
         const results = await benchmarkNodeIndexCalls(60_000_000);
         console.log(`Performance: ${results.millionRunsPerSecond.toFixed(4)} million calls per second`);
 
@@ -27,6 +50,8 @@ async function runBenchmark() {
             averageDuration: jsResults.averageDuration,
             totalDuration: jsResults.totalDuration
         });
+
+
     } catch (error) {
         console.error('Benchmark failed:', error.message);
     }
